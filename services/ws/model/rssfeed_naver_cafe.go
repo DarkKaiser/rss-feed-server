4c25c9c0ba09b4f1dab5f2425db38ef102173dd4
darkkaiser <darkkaiser@gmail.com>
2020-10-25 00:04:03 +0900

    .

--- services/ws/model/rssfeed_naver_cafe.go
+++ services/ws/model/rssfeed_naver_cafe.go
@@ -2,7 +2,9 @@ package model
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/darkkaiser/rss-feed-server/g"
+	"github.com/darkkaiser/rss-feed-server/notifyapi"
 	_ "github.com/mattn/go-sqlite3"
 	log "github.com/sirupsen/logrus"
 )
@@ -18,115 +20,176 @@ func NewNaverCafeRSSFeed(config *g.AppConfig, db *sql.DB) *NaverCafeRSSFeed {
 		db: db,
 	}
 
-	// @@@@@
-	rssFeed.init(config)
+	if err := rssFeed.init(config); err != nil {
+		m := fmt.Sprintf("네이버 카페 관련 DB를 초기화하는 중에 치명적인 오류가 발생하였습니다.\r\n\r\n%s", err)
+
+		notifyapi.SendNotifyMessage(m, true)
+
+		log.Panic(m)
+	}
 
 	return rssFeed
 }
 
-// @@@@@
-func (f *NaverCafeRSSFeed) init(config *g.AppConfig) interface{} {
-	// db만들고 일정기간 시간이 지난 데이터 모두 삭제
-	// config에 등록된 카페 정보를 이때 등록???
+func (f *NaverCafeRSSFeed) init(config *g.AppConfig) error {
+	if err := f.createTables(); err != nil {
+		return err
+	}
 
-	f.createTable(config)
+	// 기초 데이터를 추가한다.
+	for _, c := range config.RSSFeed.NaverCafes {
+		if err := f.insertNaverCafeInfo(c.ID, c.ClubID, c.Name, c.Description, c.Url); err != nil {
+			return err
+		}
+
+		for _, b := range c.Boards {
+			if err := f.insertNaverCafeBoardInfo(c.ID, b.ID, b.Name); err != nil {
+				return err
+			}
+		}
+	}
+
+	// 일정 시간이 지난 게시물 자료를 모두 삭제한다.@@@@@ 10
+	if err := f.deleteNaverCafeArticle(10); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-// @@@@@
-func (f *NaverCafeRSSFeed) createTable(config *g.AppConfig) error {
+//noinspection GoUnhandledErrorResult
+func (f *NaverCafeRSSFeed) createTables() error {
 	//
 	// naver_cafe_info 테이블
 	//
-	stmt, err := f.db.Prepare(`
+	stmt1, err := f.db.Prepare(`
 		CREATE TABLE IF NOT EXISTS naver_cafe_info (
-			id 			VARCHAR( 30) PRIMARY KEY NOT NULL UNIQUE, 
-			clubId 		VARCHAR( 30) NOT NULL, 
-			name 		VARCHAR(100) NOT NULL, 
-			description VARCHAR(200), 
+			cafeId 		VARCHAR( 30) PRIMARY KEY NOT NULL UNIQUE,
+			clubId 		VARCHAR( 30) NOT NULL,
+			name 		VARCHAR(130) NOT NULL,
+			description VARCHAR(200),
 			url 		VARCHAR( 50) NOT NULL
 		)
 	`)
 	if err != nil {
-		// @@@@@
-		log.Fatal(err.Error())
 		return err
 	}
-	_, err = stmt.Exec()
-	stmt.Close()
+	defer stmt1.Close()
+	if _, err = stmt1.Exec(); err != nil {
+		return err
+	}
 
-	stmt, err = f.db.Prepare(`
-		CREATE UNIQUE INDEX IF NOT EXISTS naver_cafe_info_index01
-						 ON naver_cafe_info(clubId)
+	stmt2, err := f.db.Prepare(`
+		CREATE INDEX IF NOT EXISTS naver_cafe_info_index01 ON naver_cafe_info(clubId)
 	`)
 	if err != nil {
-		// @@@@@
-		log.Fatal(err.Error())
 		return err
 	}
-	_, err = stmt.Exec()
+	defer stmt2.Close()
+	if _, err = stmt2.Exec(); err != nil {
+		return err
+	}
 
 	//
 	// naver_cafe_board_info 테이블
 	//
-	stmt, err = f.db.Prepare(`
+	stmt3, err := f.db.Prepare(`
 		CREATE TABLE IF NOT EXISTS naver_cafe_board_info (
-			id 			VARCHAR(  7) PRIMARY KEY NOT NULL UNIQUE,
-			name 		VARCHAR(100) NOT NULL,
 			cafeId 		VARCHAR( 30) NOT NULL,
-			FOREIGN KEY (cafeId) REFERENCES naver_cafe_info(id)
+			boardId		VARCHAR(  5) PRIMARY KEY NOT NULL UNIQUE,
+			name 		VARCHAR(130) NOT NULL,
+			FOREIGN KEY (cafeId) REFERENCES naver_cafe_info(cafeId)
 		)
 	`)
 	if err != nil {
-		// @@@@@
-		log.Fatal(err.Error())
 		return err
 	}
-	_, err = stmt.Exec()
+	defer stmt3.Close()
+	if _, err = stmt3.Exec(); err != nil {
+		return err
+	}
 
-	// 기초 데이터 추가
-	for _, c := range config.RSSFeed.NaverCafes {
-		f.AddCafeInfo(c.ID, c.ClubID, c.Name, c.Description, c.Url)
-
-		for _, b := range c.Boards {
-			f.AddCafeBoardInfo(b.ID, b.Name, c.ID)
-		}
+	stmt4, err := f.db.Prepare(`
+		CREATE INDEX IF NOT EXISTS naver_cafe_board_info_index01 ON naver_cafe_board_info(cafeId)
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt4.Close()
+	if _, err = stmt4.Exec(); err != nil {
+		return err
 	}
 
 	//
 	// naver_cafe_article 테이블
 	//
+	stmt5, err := f.db.Prepare(`
+		CREATE TABLE IF NOT EXISTS naver_cafe_article (
+			cafeId 		VARCHAR( 30) NOT NULL,
+			boardId 	VARCHAR(  5) NOT NULL,
+			articleId 	INTEGER NOT NULL,
+			title 		VARCHAR(400) NOT NULL,
+			content		TEXT,
+			link 		VARCHAR(1000) NOT NULL,
+			author 		VARCHAR(50),
+			createdAt	DATETIME,
+			PRIMARY KEY (cafeId, boardId, articleId)
+			FOREIGN KEY (cafeId) REFERENCES naver_cafe_info(cafeId)
+			FOREIGN KEY (boardId) REFERENCES naver_cafe_board_info(boardId)
+		)
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt5.Close()
+	if _, err = stmt5.Exec(); err != nil {
+		return err
+	}
+
+	stmt6, err := f.db.Prepare(`
+		CREATE INDEX IF NOT EXISTS naver_cafe_article_index01 ON naver_cafe_article(createdAt)
+	`)
+	if err != nil {
+		return err
+	}
+	defer stmt6.Close()
+	if _, err = stmt6.Exec(); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-// @@@@@
-func (f *NaverCafeRSSFeed) AddCafeInfo(id, clubId, name, description, url string) error {
-	tx, _ := f.db.Begin()
-	stmt, _ := tx.Prepare("INSERT OR REPLACE INTO naver_cafe_info (id, clubId, name, description, url) values (?, ?, ?, ?, ?)")
-	_, err := stmt.Exec(id, clubId, name, description, url)
+//noinspection GoUnhandledErrorResult
+func (f *NaverCafeRSSFeed) insertNaverCafeInfo(cafeId, clubId, name, description, url string) error {
+	stmt, err := f.db.Prepare("INSERT OR REPLACE INTO naver_cafe_info (cafeId, clubId, name, description, url) values (?, ?, ?, ?, ?)")
 	if err != nil {
-		tx.Rollback()
-		log.Println(err.Error())
 		return err
 	}
-	tx.Commit()
+	defer stmt.Close()
+	if _, err = stmt.Exec(cafeId, clubId, name, description, url); err != nil {
+		return err
+	}
 
 	return nil
 }
 
-// @@@@@
-func (f *NaverCafeRSSFeed) AddCafeBoardInfo(id, name, cafeId string) error {
-	tx, _ := f.db.Begin()
-	stmt, _ := tx.Prepare("INSERT OR REPLACE INTO naver_cafe_board_info (id, name, cafeId) values (?, ?, ?)")
-	_, err := stmt.Exec(id, name, cafeId)
+//noinspection GoUnhandledErrorResult
+func (f *NaverCafeRSSFeed) insertNaverCafeBoardInfo(cafeId, boardId, name string) error {
+	stmt, err := f.db.Prepare("INSERT OR REPLACE INTO naver_cafe_board_info (cafeId, boardId, name) values (?, ?, ?)")
 	if err != nil {
-		log.Println(err.Error())
 		return err
 	}
-	tx.Commit()
+	defer stmt.Close()
+	if _, err = stmt.Exec(cafeId, boardId, name); err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func (f *NaverCafeRSSFeed) deleteNaverCafeArticle(checkDaysAgo int) error {
+	// @@@@@ cleanOutOfLogFiles
 	return nil
 }
 
