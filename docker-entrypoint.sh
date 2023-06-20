0d10790eb0d088409c17b2820e431f78806db1b4
DarkKaiser <darkkaiser@gmail.com>
2023-06-20 12:04:41 +0900

    도커가 실행될 때 설정파일의 권한이 유지되도록 변경

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
 
-APP_BIN_FILE=/docker-entrypoint/dist/rss-feed-server
-APP_CONFIG_FILE=/docker-entrypoint/dist/rss-feed-server.json
+APP_PATH=/usr/local/app/
+APP_CONFIG_FILE=/usr/local/app/rss-feed-server.json
 
-if [ -f "$APP_BIN_FILE" ]; then
-  mv -f $APP_BIN_FILE /usr/local/app/
+LATEST_APP_BIN_FILE=/docker-entrypoint/dist/rss-feed-server
+LATEST_APP_CONFIG_FILE=/docker-entrypoint/dist/rss-feed-server.json
+
+if [ -f "$LATEST_APP_BIN_FILE" ]; then
+  mv -f $LATEST_APP_BIN_FILE $APP_PATH
 fi
 
-if [ -f "$APP_CONFIG_FILE" ]; then
-  mv -f $APP_CONFIG_FILE /usr/local/app/
+if [ -f "$LATEST_APP_CONFIG_FILE" ]; then
+  mv -f $LATEST_APP_CONFIG_FILE $APP_PATH
+  chown +1000:staff $APP_CONFIG_FILE
 fi
 
 exec "$@"
